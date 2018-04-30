e5e9879e93fc9f542b0da58a77f99463677120e7
Llewellyn Falco <isidore@setgame.com>
2018-04-30 15:40:36 -0500

    bottlesofbeer

--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
 const assert = require('assert');
-const Lesson1Straight = require('../src/Lesson1Straight');
+const lesson1 = require('../src/Lesson1Straight');
+const lesson2 = require('../src/Lesson2Variable');
 
 
 describe('RegressionTest', () => {
     it('test cat song', () => {
-        const song = new Lesson1Straight.Lesson1Straight();
+        const song = new lesson1.Lesson1Straight();
         song.singCatSong();
+        console.log(song.song);
+       // Approvals.verify(song.song);
+    });
+    it('test beer', () => {
+        const song = new lesson2.Lesson2Variable();
+        song.singBottlesOfBeer();
         console.log(song.song);
        // Approvals.verify(song.song);
     });
