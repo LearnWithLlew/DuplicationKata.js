8eb7f98e1318e05cc3b91b00c8bf2925088cc971
Llewellyn Falco <isidore@setgame.com>
2018-04-30 15:54:25 -0500

    Lesson21

--- test/test.js
+++ test/test.js
@@ -1,18 +1,28 @@
 const assert = require('assert');
 const lesson1 = require('../src/Lesson1Straight');
 const lesson2 = require('../src/Lesson2Variable');
+const lesson21 = require('../src/Lesson21');
 
 
 describe('RegressionTest', () => {
     it('test cat song', () => {
         const song = new lesson1.Lesson1Straight();
         song.singCatSong();
-        console.log(song.song);
+        //console.log(song.song);
        // Approvals.verify(song.song);
     });
     it('test beer', () => {
         const song = new lesson2.Lesson2Variable();
         song.singBottlesOfBeer();
+        //console.log(song.song);
+       // Approvals.verify(song.song);
+    });
+    it('test lesson21', () => {
+        const song = new lesson21.Lesson21();
+        const names = ["Llewellyn", "Samatha", "Tomas", "Emilia"];
+        song.singSong(1, names);
+        song.singSong(2, names);
+        song.singSong(3, names);
         console.log(song.song);
        // Approvals.verify(song.song);
     });
